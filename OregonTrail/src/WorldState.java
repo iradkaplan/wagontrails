e1681594d7ae51d15060ec38c7fbccefb32aa5d4
iradkaplan@gmail.com <iradkaplan@gmail.com@385b01f7-6b52-46ad-6f0f-ed08525db402>
2011-10-03 22:25:46 +0000

    I think WorldState is working now. Jesus Christ.

---
+++ OregonTrail/src/WorldState.java
@@ -0,0 +1,64 @@
+
+/**
+ * @author Ira
+ *
+ */
+public class WorldState {
+	private TravelGroup travelGroup;
+	private int month, dayOfMonth;
+	
+	/**
+	 * @param month the current month
+	 * @param dayOfMonth the current day
+	 * @param travelGroup the travelGroup 
+	 */
+	public WorldState(int month, int dayOfMonth, TravelGroup travelGroup){
+		this.month = month;
+		this.dayOfMonth = dayOfMonth;
+		this.travelGroup = travelGroup;
+	}
+	
+	/**
+	 * @return the TravelGroup
+	 */
+	public TravelGroup getTravelGroup() {
+		return travelGroup;
+	}
+
+	/**
+	 * @param travelGroup the TravelGroup
+	 */
+	public void setTravelGroup(TravelGroup travelGroup) {
+		this.travelGroup = travelGroup;
+	}
+
+	/**
+	 * @return the current month
+	 */
+	public int getMonth() {
+		return month;
+	}
+
+	/**
+	 * @param month the new month
+	 */
+	public void setMonth(int month) {
+		this.month = month;
+	}
+
+	/**
+	 * @return the current day
+	 */
+	public int getDayOfMonth() {
+		return dayOfMonth;
+	}
+
+	/**
+	 * @param dayOfMonth the new day
+	 */
+	public void setDayOfMonth(int dayOfMonth) {
+		this.dayOfMonth = dayOfMonth;
+	}
+	
+
+}
