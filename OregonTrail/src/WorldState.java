57e3e652652e2e7789ab79b4051ca85205c91835
ram.ramya@gmail.com <ram.ramya@gmail.com@385b01f7-6b52-46ad-6f0f-ed08525db402>
2011-10-03 22:20:25 +0000

    Added to WorldState. Also, testing SVN.

--- OregonTrail/src/WorldState.java
+++ OregonTrail/src/WorldState.java
@@ -1,6 +1,9 @@
 
 public class WorldState {
 	private TravelGroup travelGroup;
-	private 
+	private Location start;
+	private Location travelGroupLocation;
+	private int month;
+	private int dayOfMonth;
 
 }
