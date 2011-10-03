d36ef6a69e9987d79fa7bc6dccebefea7566c345
Magicjarvis@gmail.com <Magicjarvis@gmail.com@385b01f7-6b52-46ad-6f0f-ed08525db402>
2011-10-03 22:24:24 +0000

    added menu it sucks

---
+++ OregonTrail/src/Menu.java
@@ -0,0 +1,26 @@
+public class Menu {
+	private WorldState worldstate;
+	
+	
+	public void startUpMenu(String playerName, List<String> partyNames, Profession prof, Pace pace, Rations ration) {
+		
+	}  
+
+	public void updateDisplay();
+        
+
+
+
+	public void trailMenu();
+	public void fortMenu();
+	public void storeMenu();
+	public void riverMenu();
+	public void winGameMenu();
+	public void loseGameMenu();
+
+	public void gameSave();
+	public void gameLoad();
+
+
+	
+}
