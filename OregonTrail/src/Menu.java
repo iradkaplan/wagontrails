e0b88973cc3e44ff50caaa4a53839991cfd8b91c
ram.ramya@gmail.com <ram.ramya@gmail.com@385b01f7-6b52-46ad-6f0f-ed08525db402>
2011-10-03 23:35:39 +0000

    Passing in parameters from Display in Menu.

--- OregonTrail/src/Menu.java
+++ OregonTrail/src/Menu.java
@@ -1,11 +1,27 @@
-import java.util.List;
-
+import java.util.*;
 public class Menu {
 	private WorldState worldstate;
+	private String playerName;
+	private List<String> partyNames;
+	private Profession prof;
+	private TravelPace pace;
+	private RationLevel ration;
 	
+	public Menu(){
+		this.worldstate=null;
+		this.playerName=null;
+		this.partyNames=null;
+		this.prof=null;
+		this.pace=null;
+		this.ration=null;
+	}
 	
 	public void startUpMenu(String playerName, List<String> partyNames, Profession prof, TravelPace pace, RationLevel ration) {
-		
+		this.playerName=playerName;
+		this.partyNames=partyNames;
+		this.prof=prof;
+		this.pace=pace;
+		this.ration=ration;
 	}  
 
 	public void updateDisplay(){
